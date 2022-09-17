c7f311bf3a68a623a01685f9e2d50ce4e978544b
Jesse Stimpson <jesse@miterbox.io>
2022-09-17 08:45:12 -0400

    receiver intro section

--- lib/livesecret_web/live/page_live.ex
+++ lib/livesecret_web/live/page_live.ex
@@ -72,7 +72,12 @@ defmodule LiveSecretWeb.PageLive do
           </div>
 
           <% :receiver -> %>
-            <.receiver_intro />
+            <% user = if is_nil(@current_user), do: nil, else: @users[@current_user.id] %>
+
+            <%= unless is_nil(user) do %>
+              <.receiver_intro user={user} burned_at={@burned_at} live?={@live?} />
+            <% end %>
+
             <.section_header>Online now</.section_header>
             <div class="py-4">
 
@@ -92,11 +97,39 @@ defmodule LiveSecretWeb.PageLive do
 
   defp receiver_intro(assigns) do
     ~H"""
-    <div class="pt-8 px-8 pb-2 w-full flex justify-center items-center align-center">
-    Some help text
-    If burned, say so
-    If locked, wait
-    If unlocked, refresh
+    <div class="pt-8 px-8 pb-2 w-full flex justify-center items-center align-center text-gray-700">
+    <%= case {not is_nil(@burned_at), @user.state} do %>
+    <% {true, _} -> %>
+    <% {_, :locked} -> %>
+        <h3 class="text-sm font-medium text-gray-900"></h3>
+        <div class="rounded-md bg-yellow-50 p-4">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <!-- Heroicon name: mini/exclamation-triangle -->
+              <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <div class="flex">
+              <h3 class="text-sm font-medium text-yellow-800">Please wait</h3>
+              <div role="status" class="pl-2">
+                <svg aria-hidden="true" class="mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-gray-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+              </div>
+              <div class="mt-2 text-sm text-yellow-700">
+                <p>The creator of the secret link must unlock your page.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <% _ -> %>
+    <% end %>
     </div>
     """
   end
@@ -230,8 +263,9 @@ defmodule LiveSecretWeb.PageLive do
                 <.action_icon has_text={true} id={:clipboard} />
                 </button>
               </div>
-              <div class="">
-                <p class="text-sm text-gray-500">Success! When you leave this window, the content is gone forver.</p>
+              <div class="block">
+                <p class="text-md text-gray-700">Success!</p>
+                <p class="text-sm text-gray-500">When you leave this window, the content is gone forver.</p>
               </div>
             </div>
           </div>
